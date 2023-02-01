02b56d82064d2e960389a8ad45b16a2756da0891
Arthur Ikomb <ikar700@protonmail.com>
2023-02-01 11:25:38 +0100

    2nd commit

--- program/main.py
+++ program/main.py
@@ -1,4 +1,10 @@
-
+from func_connections import connect_dydx
 
 if __name__ == "__main__":
-    print("Hello")
+    # Connect to client
+    try:
+        client = connect_dydx()
+    except Exception as e:
+        print(e)
+        print("Error connecting to client", e)
+        exit(1)
