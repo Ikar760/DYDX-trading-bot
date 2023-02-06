48bbdbcef05524ebdf1db0ef653db0619c2f6431
Arthur Ikomb <ikar700@protonmail.com>
2023-02-06 10:26:22 +0100

    stage2 starts

--- program/func_private.py
+++ program/func_private.py
@@ -11,24 +11,26 @@ def place_market_order(client, market, side, size, price, reduce_only):
     account_response = client.private.get_account()
     position_id = account_response.data["account"]["positionId"]
 
-    # Get expiration time
+     # Get expiration time
     server_time = client.public.get_time()
-    expiration = datetime.fromisoformat((server_time.data["iso"].replace("Z","")) + timedelta(seconds=70))
-
-    # Place an order
+    expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(seconds=2000)
+ 
+  # Place an order
+  # expiration_epoch_seconds=expiration.timestamp(),
     placed_order = client.private.create_order(
-        position_id=position_id, # required for creating the order signature
-        market=market,
-        side="BUY",
-        order_type="MARKET",
-        post_only=False,
-        size=size,
-        price=price, #above current price
-        limit_fee='0.015',
-        expiration_epoch_seconds=expiration.timestamp(),
-        time_in_force="FOK", #fill or kill
-        reduce_only=reduce_only #changes the position from close to open
-    )
+    position_id=position_id, # required for creating the order signature
+    market=market,
+    side=side,
+    order_type="MARKET",
+    post_only=False,
+    size=size,
+    price=price,
+    limit_fee='0.015',
+ 
+    expiration_epoch_seconds=time.time() + 65,
+    time_in_force="FOK", 
+    reduce_only=reduce_only
+  )
 
     # Return result
     return placed_order.data
@@ -74,7 +76,7 @@ def abort_all_positions(client):
             
             # Get Price
             price = float(position["entryPrice"])
-            accept_price = price * 1.7 if side == "BUY" else price * 0.3 #if price is 70% wrst than when opening position
+            accept_price = price * 1.7 if side == "BUY" else price * 0.3 #if price is 70% worst than when opening position
             tick_size = markets["markets"][market]["tickSize"]
             accept_price = format_number(accept_price, tick_size)
 
@@ -94,5 +96,5 @@ def abort_all_positions(client):
             # Protect API
             time.sleep(0.2)
 
-            # Return close orders
-            return close_orders
+        # Return close orders
+        return close_orders
--- program/main.py
+++ program/main.py
@@ -1,10 +1,34 @@
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 from func_connections import connect_dydx
+from func_private import abort_all_positions
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
     # Connect to client
     try:
+        print("Connecting to client...")
         client = connect_dydx()
     except Exception as e:
         print(e)
         print("Error connecting to client", e)
         exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_order = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions", e)
+            exit(1)
+    
+    # Find Cointegrated pairs
+    if FIND_COINTEGRATED:
+
+        # Construct market prices
+        try:
+            print("Fetching market prices..")
+            close_order = abort_all_positions(client)
+        except Exception as e:
+            print("Error constructing market prices", e)
+            exit(1)
